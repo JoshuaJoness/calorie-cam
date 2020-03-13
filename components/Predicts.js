3cb4615ec5c00210264f2effa9a99d66ae3a9ab6
Joshua Jones <joshuajones@trentu.ca>
2020-03-13 13:16:08 -0400

    changes to make work

--- components/Predicts.js
+++ components/Predicts.js
@@ -1,7 +1,8 @@
 import React, {useState} from 'react'
-import { StyleSheet, Text, TouchableOpacity, View, Image, ScrollView, TouchableHighlight, Modal } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View, Image, ScrollView, TouchableHighlight, Modal, StatusBar } from 'react-native'
 import * as ImagePicker from 'expo-image-picker'
 import * as Permissions from 'expo-permissions'
+import { Ionicons, FontAwesome } from '@expo/vector-icons';
 import axios from 'axios'
 // Require the client
 
@@ -14,18 +15,25 @@ const app = new Clarifai.App({
 });
 
 const Predicts = () => {
-	const [image, setImage] = useState('')
-	const [resultImage, setResultImage] = useState('')
-	const [modalVisible, setModalVisible] = useState(false);
+	Predicts.navigationOptions = {
+		headerShown: false
+		};
+	const [image, setImage] = useState()
+	const [imageToDisplay, setImageToDisplay] = useState()
+	const [label, setLabel] = useState('')
+	const [calories, setCalories] = useState(0)
+	const [carbs, setCarbs] = useState(0)
+	const [protein, setProtein] = useState(0)
+	const [fat, setFat] = useState(0)
 
-	const selectPicture = async () => {
-    await Permissions.askAsync(Permissions.CAMERA_ROLL);
-    const { cancelled, uri } = await ImagePicker.launchImageLibraryAsync({
-      aspect: 1,
-      allowsEditing: true,
-    });
-    if (!cancelled) setImage(uri);
-  };
+	// const selectPicture = async () => {
+  //   await Permissions.askAsync(Permissions.CAMERA_ROLL);
+  //   const { cancelled, uri } = await ImagePicker.launchImageLibraryAsync({
+  //     aspect: 1,
+  //     allowsEditing: true,
+  //   });
+  //   if (!cancelled) setImage(uri);
+  // };
 
   const takePicture = async () => {
     await Permissions.askAsync(Permissions.CAMERA);
@@ -35,75 +43,69 @@ const Predicts = () => {
     });
 		// here we set the image to base64 data of the image that we took with the camera
     setImage(base64);
+		setImageToDisplay(uri)
   };
 
-	const showState = () => {
-		console.log('................',resultImage)
-	}
-
-
-		const predict = () =>	{
-			// here we send the image encoded as base 64 to the clarifai API to determine what kind of food it is
-			app.models.predict("bd367be194cf45149e75f01d59f77ba7", {base64:image}).then(
-			    function(response){
-						let tag = response.rawData.outputs[0].data.concepts[0].name;
-						alert(`this is a ${tag}`)
-						axios.get(`https://api.wolframalpha.com/v1/simple?i=${tag}&appid=9PWATX-P68ELU4YPV`)
-							.then(res => {
-								console.log('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',res);
-								console.log('*********************',res.config.url);
-								setModalVisible(true);
-								setResultImage(res.config.url)
-							}).catch(err => {
-								console.log(err);
-							})
+	const predict = () =>	{
+		// here we send the image encoded as base 64 to the clarifai API to determine what kind of food it is
+		app.models.predict("bd367be194cf45149e75f01d59f77ba7", {base64:image}).then(
+			function(response){
+				let tag = response.rawData.outputs[0].data.concepts[0].name;
+					alert(`this is a ${tag}`)
+					axios.get(`https://api.edamam.com/api/food-database/parser?app_id=7ff1ee7e&app_key=aa4824adda205d7ff601301c08816573&ingr=${tag}`)
+						.then(res => {
+							console.log('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',res.data.hints[0].food.label,res.data.hints[0].food.nutrients.ENERC_KCAL, res.data.hints[0].food.nutrients.CHOCDF,res.data.hints[0].food.nutrients.PROCNT,res.data.hints[0].food.nutrients.FIBTG)
+							setLabel(res.data.hints[0].food.label)
+							setCalories(res.data.hints[0].food.nutrients.ENERC_KCAL)
+							setCarbs(res.data.hints[0].food.nutrients.CHOCDF)
+							setProtein(res.data.hints[0].food.nutrients.PROCNT)
+							setFat(res.data.hints[0].food.nutrients.FIBTG)
+						})
+						.catch(err => {
+							console.log(err)})
 			    },
 			    function(err){
 			      console.log(err)
 			    }
-			)
-		}
+				)
+			}
 
 	return(
 		<View style={styles.container}>
-
-			<Image style={styles.image} source={{ uri: image }} />
+			<StatusBar barStyle='dark-content'/>
+			<Image style={styles.image} source={{ uri: imageToDisplay }} />
 			<View style={styles.row}>
-				<Button onPress={selectPicture}>Gallery</Button>
 				<Button onPress={takePicture}>Camera</Button>
-				<Button onPress={predict.bind(this)} >Submit</Button>
-				<Button onPress={showState}>State</Button>
+				<Button onPress={predict.bind(this)}>Submit</Button>
 			</View>
-
-				<Image
-		          style={{ width: 300, height: 500 }}
-		          source={{ uri: resultImage }}
-		        />
-
-						<Modal
-								animationType="slide"
-								transparent={false}
-								visible={modalVisible}
-								onRequestClose={() => {
-									Alert.alert('Modal has been closed.');
-								}}>
-								<View style={{ marginTop: 22 }}>
-          <View style={{ width: '100%', height: '100%', flex:1 }}>
-					<Image
-								style={{ width: 400, height: 700 }}
-								source={{ uri: resultImage }}
-							/>
-
-            <TouchableHighlight
-              onPress={() => {
-                setModalVisible(!modalVisible);
-              }}>
-              <Text style={{color:'black'}}>Hide Modal</Text>
-            </TouchableHighlight>
-          </View>
-        </View>
-      </Modal>
-
+			{
+				!label ?
+				<View style={{marginTop:35}}>
+					<Text style={{fontWeight:'bold', fontSize:15}}>1. Press 'Camera' to take a picture of your food!</Text>
+					<Text style={{fontWeight:'bold', fontSize:15, marginTop:5}}>2. Press 'Submit' to retrieve nutritional information!</Text>
+				</View>
+				:
+				<View style={{marginTop:35}}>
+					<TouchableHighlight
+						onPress={() => {
+							setImage()
+							setImageToDisplay()
+							setLabel('')
+							setCarbs(0)
+							setProtein(0)
+							setFat(0)
+							setCalories(0)
+						}}
+						style={{marginTop:10, marginLeft: 'auto'}}>
+						<Ionicons name="md-close" size={26} />
+					</TouchableHighlight>
+					<Text style={{fontSize:18}}><Text style={{fontWeight:'bold', fontSize:18}}>Label:</Text> {label}</Text>
+					<Text style={{fontSize:18, marginTop:10}}><Text style={{fontWeight:'bold', fontSize:18}}>Calories: </Text>{calories} per serving</Text>
+					<Text style={{fontSize:18, marginTop:10}}><Text style={{fontWeight:'bold', fontSize:18}}>Protein:</Text> {Math.round(protein)}</Text>
+					<Text style={{fontSize:18, marginTop:10}}><Text style={{fontWeight:'bold', fontSize:18}}>Carbs:</Text> {Math.round(carbs)}</Text>
+					<Text style={{fontSize:18, marginTop:10}}><Text style={{fontWeight:'bold', fontSize:18}}>Fat:</Text> {Math.round(fat)}</Text>
+				</View>
+			}
 		</View>
 	)
 }
@@ -115,23 +117,16 @@ const Button = ({ onPress, children }) => (
 );
 
 const styles = StyleSheet.create({
-  text: {
-    fontSize: 21,
-  },
+  text: { fontSize: 21 },
   row: { flexDirection: 'row' },
   image: { width: 300, height: 300, backgroundColor: 'gray' },
-  button: {
-    padding: 13,
-    margin: 15,
-    backgroundColor: '#dddddd',
-  },
-  container: {
-    flex: 1,
-    backgroundColor: '#ffffff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  button: { padding: 13, margin: 15, backgroundColor: '#dddddd', borderRadius:4 },
+  container: { flex: 1, backgroundColor: '#F3FFC6', alignItems: 'center', 		   justifyContent: 'center'},
 });
 
 
 export default Predicts
+
+// INCLUDE AMOUNT IN QUERY
+
+// <Button onPress={selectPicture}>Gallery</Button>
--- components/Welcome.js
+++ components/Welcome.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, Image, Animated, TextInput, StyleSheet } from 'react-native'
+import { View, Image, Animated, TextInput, StyleSheet, StatusBar } from 'react-native'
 import { Container, Header, Content, Icon, Picker, Form, Button, Text  } from "native-base";
 import { AppLoading } from 'expo';
 import * as Font from 'expo-font';
@@ -8,16 +8,12 @@ import { Ionicons } from '@expo/vector-icons';
 class Welcome extends React.Component {
 	static navigationOptions = {
 		title: "iRecomp",
-		headerStyle: {
-			backgroundColor:'#000000'
-		},
-		headerTitleStyle: {
-			color: '#D3F09C'
-		}
+		headerShown: false
 	}
 
 	state={
-		isReady:null
+		isReady:null,
+		time:0
 	}
 
 	async componentDidMount(){
@@ -28,6 +24,19 @@ class Welcome extends React.Component {
 			...Ionicons.font,
 		});
 		this.setState({ isReady: true });
+
+		let time = this.state.time
+		setTimeout(function(){
+			time = 1
+      this.setState({time:1});
+		}.bind(this),1000);
+
+		setTimeout(function(){
+			time = 2
+		  this.setState({time:2});
+		}.bind(this),1000);
+
+
 	}
 
 	goToSearch = () => {
@@ -41,15 +50,17 @@ render() {
 	 }
 	return(
 		<View style={styles.container}>
-			<Image
-				style={styles.image}
-				source={require('../assets/beet.png')}
-			/>
+			<StatusBar barStyle='dark-content'/>
+			<Text style={{fontFamily:'Pacifico', fontSize:60, marginTop:200, marginLeft:'auto', marginRight:'auto',marginBottom:40, color:"#FF7FAA"}}>Food Tracker</Text>
+				<Image
+					style={styles.image}
+					source={require('../assets/beet.png')}
+				/>
 			<Button
-				style={{backgroundColor:'#1C5253', width:200, marginLeft:'auto', marginRight:'auto', borderWidth:2, borderColor:'#306B34', color:'#F3FFC6', marginTop:60}}
+				style={{backgroundColor:'#FF7FAA', width:200, marginLeft:'auto', marginRight:'auto', marginTop: 50}}
 				onPress={this.goToSearch}
 				>
-				<Text>Log your first meal!</Text>
+				<Text style={{color:'#FFF', fontWeight:'bold'}}>Log your first meal!</Text>
 			</Button>
 		</View>
 	)}
@@ -59,15 +70,15 @@ export default Welcome
 
 const styles = StyleSheet.create ({
 	container:{
-		backgroundColor:"#D3F09C",
-		height:1000
+		backgroundColor:"#F3FFC6",
+		height:900
 	},
 	image: {
 		height:150,
 		width:150,
 		marginLeft:'auto',
 		marginRight:'auto',
-		marginTop: 200
+
 	},
 	text: {
 		fontSize:18, marginLeft:'auto', marginRight:'auto', marginTop:'10%'
